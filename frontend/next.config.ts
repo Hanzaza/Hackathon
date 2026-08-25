94534956ee13e1308e5c55b020ad0a853a0aef72
Javier Pastora <javierpastora031004@gmail.com>
2026-08-25 10:53:02 -0600

    URL and Typed errors fixed

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.1.25', '192.168.1.25:3000', 'localhost:3000'],
+  turbopack: {
+    root: path.resolve(__dirname, '..'),
+  },
   images: {
     remotePatterns: [
       {
